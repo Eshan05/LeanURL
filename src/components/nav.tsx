1f30ebd073da4cb21705952a8e9584f9e17662ab
Eshan05 <nahareshan05@gmail.com>
2024-12-08 23:44:43 +0530

    Authentication for graphs page

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ChartArea, HomeIcon } from "lucide-react";
+import { ChartArea, HomeIcon, UserCog2 } from "lucide-react";
 import { ThemeToggle } from "@components/themeToggle";
 import Link from "next/link";
 
@@ -9,10 +9,11 @@ export const Nav = () => {
     <nav
       className='flex px-6 py-3 justify-between bg-[#ccc2] dark:bg-[#8882] backdrop-blur-md shadow-lg mt-8 absolute rounded-full lg:w-1/3 top-0 w-4/5 z-20'>
       <h2 className='font-bold '> LeanURL </h2>
-      <section className='flex space-x-4'>
+      <section className='flex space-x-3'>
         <ThemeToggle />
         <Link href='/share' className='hover:text-blue-500'><HomeIcon className="w-5 h-5" /></Link>
-        <Link href='/analytics' className='hover:text-blue-500'><ChartArea className="w-5 h-5" /></Link>
+        <Link href='/analytics' className='hover:text-blue-500'><UserCog2 className="w-5 h-5" /></Link>
+        <Link href='/graphs' className='hover:text-blue-500'><ChartArea className="w-5 h-5" /></Link>
       </section>
     </nav>
   );
