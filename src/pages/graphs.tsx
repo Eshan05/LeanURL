963e01787d65f264f478f5f51854d4f9715b44a2
Eshan05 <nahareshan05@gmail.com>
2024-12-10 11:38:01 +0530

    Make visualize header component to help with loading
    - 3G: 3s, 4G: Sub 1s (Without cache: 4s)
    - Analytics: 3G: Sub 4s, 4G: 1.5s (Without cache: 6s)
    - Home: 3G: 1s, 4G: .5s (Without cache: 4s)

--- src/pages/graphs.tsx
+++ src/pages/graphs.tsx
@@ -10,9 +10,8 @@ import { URLDocument, URLWithDuplicateCount } from "@/types/types";
 import { ChevronDown, ExternalLinkIcon, LinkIcon, RefreshCcw, SearchIcon } from "lucide-react";
 import { useEffect, useState } from "react";
 import { Checkbox } from "@/components/ui/checkbox";
-import ColorPicker from "@/components/colorPicker";
-import { generateAnalogousColors, generateHeatmapColors, generateTreemapColors } from "@/lib/utils/utils";
 
+const VisualizeHeader = dynamic(() => import("@/components/visualizeHeader"), { ssr: false });
 const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
 interface ChartOptions {
@@ -235,100 +234,17 @@ const Visualize: React.FC = () => {
 
         <div className="w-full px-[1.15rem] py-10 mx-auto lg:px-8 lg:py-16">
           <p className='mb-2 font-mono text-center small-caps c-beige:text-beige-800'>LeanURL</p>
-          <header className="relative flex flex-col items-center justify-center w-full mb-10 space-y-10 overflow-hidden">
-            <h1 className="text-4xl font-extrabold tracking-tight scroll-m-20 lg:text-5xl c-beige:text-beige-800">
-              Visualize
-            </h1>
-            <section className="flex items-center justify-center space-x-2">
-              <Button type="button" variant="outline" className="flex" onClick={handleSearchMobile}>
-                <span className="flex w-4 aspect-square">
-                  <SearchIcon />
-                </span>
-              </Button>
-              <Select
-                value={selectedUrl?._id || ""}
-                onValueChange={(value: string) =>
-                  setSelectedUrl(urls.find((url) => url._id === value) || null)
-                }
-              >
-                <SelectTrigger className="lg:max-w-[50%] min-w-[200px] w-max max-w-[300px] focus-visible:ring-0 focus-visible:outline-none focus:ring-0 focus:ring-offset-0">
-                  <SelectValue placeholder="Select URL" />
-                  <SelectIcon asChild>
-                    <ChevronDown className='w-4 h-4 ml-4 opacity-50' />
-                  </SelectIcon>
-                </SelectTrigger>
-                <SelectContent>
-                  {urls.map((url) => (
-                    <SelectItem key={url._id} value={url._id}>
-                      {url.shortenUrl}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-            </section>
-            <section className="flex items-center justify-center my-2 space-x-4 *:items-center">
-              <div className="flex flex-col">
-                <ColorPicker
-                  value={options.areaChartColor}
-                  onColorChange={(color: string) => {
-                    setOptions((options) => ({
-                      ...options,
-                      areaChartColor: color,
-                      areaChartColors: generateAnalogousColors(color),
-                    }));
-                  }}
-                />
-                <p className="font-mono text-xs text-muted-foreground">Area</p>
-              </div>
-
-              <div className="flex flex-col">
-                <ColorPicker
-                  value={options.treemapColor}
-                  onColorChange={(color: string) => {
-                    setOptions((options) => ({
-                      ...options,
-                      treemapColor: color,
-                      treemapColors: generateTreemapColors(color),
-                    }));
-                  }}
-                />
-                <p className="font-mono text-xs text-muted-foreground">Tree</p>
-              </div>
-
-              <div className="flex flex-col">
-                <ColorPicker
-                  value={options.heatmapColor}
-                  onColorChange={(color: string) => {
-                    setOptions((options) => ({
-                      ...options,
-                      heatmapColor: color,
-                      heatmapColors: generateHeatmapColors(color),
-                    }));
-                  }}
-                />
-                <p className="font-mono text-xs text-muted-foreground">Heat</p>
-              </div>
-
-              <div className="flex flex-col">
-                <ColorPicker
-                  value={options.radarChartColor}
-                  onColorChange={(color: string) => {
-                    setOptions((options) => ({
-                      ...options,
-                      radarChartColor: color, // Only base color for radar
-                    }));
-                  }}
-                />
-                <p className="font-mono text-xs text-muted-foreground">Radar</p>
-
-              </div>
-            </section>
-            {selectedUrl &&
-              <footer className="*:flex *:items-center *:space-x-4 flex flex-col space-y-2 text-xs">
-                <p><LinkIcon className="w-5 h-5" /><span className="inline-block px-3 py-1.5 font-mono border rounded-lg text-primary hover:underline overflow-x-auto w-[128px] scrollbar-none whitespace-nowrap c-beige:text-beige-700">{selectedUrl?.shortenUrl}</span></p>
-                <p><ExternalLinkIcon className="w-5 h-5" /><span className="inline-block px-3 py-1.5 font-mono border rounded-lg text-primary hover:underline overflow-x-auto w-[128px] scrollbar-none whitespace-nowrap c-beige:text-beige-700">{selectedUrl?.originalUrl}</span></p>
-              </footer>}
-          </header>
+          {!loading && (
+            <VisualizeHeader
+              urls={urls}
+              selectedUrl={selectedUrl}
+              onSearchMobile={handleSearchMobile}
+              onUrlSelect={setSelectedUrl}
+              options={options}
+              setOptions={setOptions}
+            />
+          )}
+          {/* The above has the select URL feature, the search button for mobile, the color pickers which update options (And thank god work) and it updates the selected URL via onUrlSelect and also an event is emitted from it via onSearchMobile. Also shows the link footer */}
           {loading &&
             <div className='flex items-center justify-center w-full py-5'>
               <Image src="/images/bars-scale.svg"
@@ -444,11 +360,11 @@ const Visualize: React.FC = () => {
                               useFillColorAsStroke: false,
                               colorScale: {
                                 ranges: [
-                                  { from: 1, to: 20, color: options.heatmapColors[0], name: 'Low' },
-                                  { from: 21, to: 40, color: options.heatmapColors[1], name: 'Medium' },
-                                  { from: 41, to: 60, color: options.heatmapColors[2], name: 'High' },
-                                  { from: 61, to: 80, color: options.heatmapColors[3], name: 'Century' },
-                                  { from: 81, to: 120, color: options.heatmapColors[4], name: 'Fire' },
+                                  { from: 1, to: 20, color: options.heatmapColors[0] || "#000000", name: 'Low' },
+                                  { from: 21, to: 40, color: options.heatmapColors[1] || "#333333", name: 'Medium' },
+                                  { from: 41, to: 60, color: options.heatmapColors[2] || "#666666", name: 'High' },
+                                  { from: 61, to: 80, color: options.heatmapColors[3] || "#999999", name: 'Century' },
+                                  { from: 81, to: 120, color: options.heatmapColors[4] || "#cccccc", name: 'Fire' },
                                 ],
                               }
                             },
